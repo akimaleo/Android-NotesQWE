0156d8b865370cd514f1fd7dcabf955f84d55731
Yaroslav <akimaleo@ya.ru>
2017-08-16 19:29:21 +0300

    initial commit

---
+++ app/src/main/java/com/letit0or1/akimaleo/notesqwe/MainActivity.java
@@ -0,0 +1,13 @@
+package com.letit0or1.akimaleo.notesqwe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
