7a5e3fa951568b49f8043a1d8a09d6379aea85fd
Yaroslav <akimaleo@ya.ru>
2017-08-22 22:06:44 +0300

    TestButtons

--- app/src/main/java/com/letit0or1/akimaleo/notesqwe/view/main/MainActivity.kt
+++ app/src/main/java/com/letit0or1/akimaleo/notesqwe/view/main/MainActivity.kt
@@ -6,9 +6,14 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
+import com.google.firebase.database.FirebaseDatabase
 import com.letit0or1.akimaleo.notesqwe.R
+import com.letit0or1.akimaleo.notesqwe.util.webdata.SyncWorker
+import com.letit0or1.akimaleo.notesqwe.util.UserDataUtil
 import com.letit0or1.akimaleo.notesqwe.view.authorization.AuthorizationActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -31,9 +36,23 @@ class MainActivity : AppCompatActivity() {
         mAdapter = NotesRecyclerViewAdapter(ArrayList())
         mRecyclerView.setAdapter(mAdapter);
 
+        get_data.setOnClickListener {
+            SyncWorker().downloadData()
+        }
+
+        write_time.setOnClickListener {
+            val database = FirebaseDatabase.getInstance()
+            var format = SimpleDateFormat("dd.MM.yyy HH:mm:ss")
+            val myRef = database.getReference(UserDataUtil.instance.firebaseAuth.currentUser?.email?.replace('.', '-', true))
+            myRef.setValue(format.format(Calendar.getInstance().time))
+
+//            SyncWorker().putData(listOf({ Note() }))
+        }
+
         login.setOnClickListener {
             val intent = Intent(this, AuthorizationActivity::class.java)
             startActivity(intent)
         }
+
     }
 }
