6deaa5d4d141fabbcae3d8ebf7928b1be2884290
Yaroslav <akimaleo@ya.ru>
2017-08-23 15:55:17 +0300

    Custom super Activity class

---
+++ app/src/main/java/com/letit0or1/akimaleo/notesqwe/view/CActivity.kt
@@ -0,0 +1,17 @@
+package com.letit0or1.akimaleo.notesqwe.view
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+/**
+ * Created by akimaleo on 23.08.17.
+ */
+
+open class CActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setSys
+//        setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_‌​BAR);
+    }
+}
