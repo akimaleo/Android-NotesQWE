da844d714981fcac03deae81da236d15bb6642b7
Yaroslav <akimaleo@ya.ru>
2017-08-23 23:31:19 +0300

    Migrated from MVC to MVP

--- app/src/main/java/com/letit0or1/akimaleo/notesqwe/view/authorization/AuthorizationActivity.kt
+++ app/src/main/java/com/letit0or1/akimaleo/notesqwe/view/authorization/AuthorizationActivity.kt
@@ -4,101 +4,35 @@ import android.app.Activity
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.View
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.*
+import com.google.firebase.auth.FirebaseAuth
 import com.letit0or1.akimaleo.notesqwe.R
-import com.letit0or1.akimaleo.notesqwe.util.FirebaseUtil
 import com.letit0or1.akimaleo.notesqwe.view.CActivity
 import kotlinx.android.synthetic.main.activity_authorization.*
 
 
-class AuthorizationActivity : CActivity() {
+class AuthorizationActivity : CActivity(), AuthorizationView {
+
 
     private lateinit var mAuth: FirebaseAuth
-    private lateinit var mAuthListener: FirebaseAuth.AuthStateListener
+    private lateinit var presenter: AuthorizationPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authorization)
-
-        mAuth = FirebaseUtil.instance.firebaseAuth
-//        mAuthListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
-//            val user = firebaseAuth.currentUser
-//            if (user != null) {
-//                // User is signed in
-////                Log.d(FragmentActivity.TAG, "onAuthStateChanged:signed_in:" + user.uid)
-//            } else {
-//                // User is signed out
-////                Log.d(FragmentActivity.TAG, "onAuthStateChanged:signed_out")
-//            }
-//        }
-//        FirebaseUtil.instance.firebase.addAuthStateListener(mAuthListener)
+        presenter = AuthorizationPresenterImpl(this)
     }
 
     public fun signIn(view: View) {
-        val emailStr = email.text.toString()
-        val passwordStr = password.text.toString()
-
-        if (!emailStr.matches(Regex("(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"))) {
-            email.error = getString(R.string.email_is_incorrect)
-        } else if (passwordStr.isEmpty()) {
-            email.error = getString(R.string.value_cant_by_blank)
-        } else {
-            mAuth.signInWithEmailAndPassword(emailStr, passwordStr)
-                    .addOnCompleteListener(this) { task ->
-                        if (task.isSuccessful) {
-                            Log.e("FIREBASE", "sight in success")
-                            successLogin()
-                        } else {
-                            if (!task.isSuccessful) {
-                                Log.e("FIREBASE", "sight in error")
-                                try {
-                                    throw task.getException()!!
-                                } catch (e: FirebaseAuthInvalidCredentialsException) {
-                                    invalidCredentials()
-                                } catch (e: FirebaseAuthInvalidUserException) {
-                                    reqRegister()
-                                }
-                            }
-                        }
-                    }
-        }
+        presenter.login(email.text.toString(), password.text.toString())
     }
 
-    private fun signUp() {
-        val emailStr = email.text.toString()
-        val passwordStr = password.text.toString()
 
-        mAuth.createUserWithEmailAndPassword(emailStr, passwordStr)
-                .addOnCompleteListener { task: Task<AuthResult> ->
-                    if (!task.isSuccessful) {
-                        try {
-                            throw task.getException()!!
-                        } catch (e: FirebaseAuthWeakPasswordException) {
-                            password.setError(getString(R.string.error_weak_password))
-                            password.requestFocus()
-                        } catch (e: FirebaseAuthInvalidCredentialsException) {
-                            email.setError(getString(R.string.error_invalid_email))
-                            email.requestFocus()
-                        } catch (e: FirebaseAuthUserCollisionException) {
-                            signUp()
-                        } catch (e: Exception) {
-                            Log.e("FIREBASE AUTH SIGN UP", e.message)
-                        }
-                    } else {
-                        successLogin()
-                    }
-                }
-    }
-
-    private fun reqRegister() {
+    override fun reqRegister() {
         val builder = AlertDialog.Builder(this)
         builder.setMessage(R.string.req_register_new_user)
                 .setPositiveButton(android.R.string.ok, DialogInterface.OnClickListener { dialog, id ->
-                    signUp()
+                    presenter.register(email.text.toString(), password.text.toString())
                 })
                 .setNegativeButton(android.R.string.cancel, DialogInterface.OnClickListener { dialog, id ->
                     // User cancelled the dialog
@@ -107,24 +41,24 @@ class AuthorizationActivity : CActivity() {
         builder.create().show()
     }
 
-    private fun invalidCredentials() {
+
+    override fun loginSuccess() {
+        setResult(Activity.RESULT_OK)
+        finish()
+    }
+
+    override fun invalidCredentials() {
         val builder = AlertDialog.Builder(this)
         builder.setMessage(R.string.invalid_credentials)
         builder.create().show()
     }
 
-    private fun successLogin() {
-        setResult(Activity.RESULT_OK)
-        finish()
+    override fun emailError(error: String) {
+        email.error = error
     }
-//
-//    public override fun onStart() {
-//        super.onStart()
-//        mAuth.addAuthStateListener(mAuthListener)
-//    }
-//
-//    public override fun onStop() {
-//        super.onStop()
-//        mAuth.removeAuthStateListener(mAuthListener)
-//    }
+
+    override fun passwordError(error: String) {
+        password.error = error
+    }
+
 }
