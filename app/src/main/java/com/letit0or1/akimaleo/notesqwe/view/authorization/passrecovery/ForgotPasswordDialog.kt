01f6722f3ea3b6d3bfb46ab52f8e91c9c1a164a3
Yaroslav <akimaleo@ya.ru>
2017-09-03 18:52:50 +0300

    Handlers added

--- app/src/main/java/com/letit0or1/akimaleo/notesqwe/view/authorization/passrecovery/ForgotPasswordDialog.kt
+++ app/src/main/java/com/letit0or1/akimaleo/notesqwe/view/authorization/passrecovery/ForgotPasswordDialog.kt
@@ -5,20 +5,22 @@ import android.content.Context
 import android.os.Bundle
 import android.widget.EditText
 import com.letit0or1.akimaleo.notesqwe.R
-import com.letit0or1.akimaleo.notesqwe.util.FirebaseUtil
 import com.letit0or1.akimaleo.notesqwe.view.authorization.AuthorizationPresenter
+import kotlinx.android.synthetic.main.dialog_password_recovery.*
 
 /**
  * Created by akimaleo on 02.09.17.
  */
 
-class ForgotPasswordDialog(context: Context, var presenter: AuthorizationPresenter) : Dialog(context) {
+class ForgotPasswordDialog(context: Context, private var presenter: AuthorizationPresenter) : Dialog(context) {
 
     override fun onCreate(savedInstanceState: Bundle) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dialog_password_recovery)
         val email = findViewById(R.id.email) as EditText
-        FirebaseUtil.instance.firebaseAuth.sendPasswordResetEmail(email.text.toString())
+
+        submit_action.setOnClickListener { presenter.restore(email.text.toString()) }
+        cancel_action.setOnClickListener { dismiss() }
     }
 
 }
