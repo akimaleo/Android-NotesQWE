cbb3017cae8d85745c45b0362f87822598ec250c
kawa <akimaleo@ya.ru>
2018-03-20 00:07:55 +0200

    Merge conflicts

--- app/src/main/java/com/letit0or1/akimaleo/notesqwe/presentation/di/FirebaseDaoModule.kt
+++ app/src/main/java/com/letit0or1/akimaleo/notesqwe/presentation/di/FirebaseDaoModule.kt
@@ -1,6 +1,6 @@
 package com.letit0or1.akimaleo.notesqwe.presentation.di
 
-import com.letit0or1.akimaleo.notesqwe.data.web.SyncWorkerImpl
+import com.letit0or1.akimaleo.notesqwe.data.web.FirebaseDaoImpl
 import dagger.Module
 import dagger.Provides
 
@@ -8,8 +8,8 @@ import dagger.Provides
  * Created by kawa on 19.03.2018.
  */
 @Module
-class SyncWorkerModule {
+class FirebaseDaoModule {
 
     @Provides
-    fun provideSyncWorker(uuid: String) = SyncWorkerImpl(uuid)
+    fun provideSyncWorker(uuid: String) = FirebaseDaoImpl(uuid)
 }
